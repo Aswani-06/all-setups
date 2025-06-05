3c79fb686520d80d95c2e560cb635d2dbf5b9aca
Aswani.M <medikondaaswani23@gmail.com>
2025-06-05 12:12:50 +0530

    Update ubuntu-minikube.sh

--- ubuntu-minikube.sh
+++ ubuntu-minikube.sh
@@ -12,3 +12,13 @@ sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.t
 sudo echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 sudo minikube start --driver=docker --force
+
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"   :-To install kubectl
+ chmod +x kubectl
+ mv kubectl /usr/local/bin/
+ll  /usr/local/bin/
+kubectl vesrion
+minikube version
+minikube status
+
+   
